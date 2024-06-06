d01b4e00e8ac71609f1427f240b0b317c53e3993
Nat Tan <tresarpnat27@gmail.com>
2024-06-06 21:53:40 +0700

    solv excersice manipulate

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import "./App.css";
 import Posts from "./components/Posts";
+import postsList from "./components/data/posts";
 
 function App() {
   return (
     <div className="App">
-      <Posts />
+      <div class="app-wrapper">
+        <h1 class="app-title">Posts</h1>
+        <div class="post-list">
+          {postsList.map((item, i) => (
+            <Posts
+              title={item.title}
+              content={item.content}
+              likes={item.likes}
+            />
+            // <div key={i}>
+            //   <div>{item.title}</div>
+            //   <div>{item.content}</div>
+            //   <div>{item.likes}</div>
+            // </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,30 +1,50 @@
-function Posts() {
+import { useState } from "react";
+
+function Posts(props) {
+  const [countLike, setCountLike] = useState(props.likes);
+  const handleAddLike = () => {
+    setCountLike(+countLike + 1);
+  };
+
+  const handleSubtractLike = () => {
+    if (countLike > 0) {
+      setCountLike(+countLike - 1);
+    } else {
+      setCountLike = 0;
+    }
+  };
+
   return (
-    <div class="app-wrapper">
-      <h1 class="app-title">Posts</h1>
-      <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
-            </div>
-          </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+    // <div class="app-wrapper">
+    //   <h1 class="app-title">Posts</h1>
+    //   <div class="post-list">
+    <div class="post-item">
+      <div class="post-header">
+        <h2>Post Title {props.title}</h2>
+        <div class="post-social-media-stats">
+          <span class="stats-topic">Likes: </span>
+          <span class="post-likes">{countLike}</span>
+        </div>
+      </div>
+      <p class="post-content">
+        {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
             vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
             tincidunt, leo non molestie consectetur, elit libero faucibus
             tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
-        </div>
+            condimentum mauris euismod pellentesque eu eu justo... */}
+        {props.content}
+      </p>
+      <div class="post-actions">
+        <button class="like-button" onClick={handleAddLike}>
+          Like
+        </button>
+        <button class="dislike-button" onClick={handleSubtractLike}>
+          Dislike
+        </button>
       </div>
     </div>
+    //   {/* </div>
+    // </div> */}
   );
 }
 
